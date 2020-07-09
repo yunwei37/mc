a1dd6c57d671685585b87bf80af7e07225bf20ea
Hanjing Zhou <15889652@qq.com>
2020-07-09 14:36:58 +0800

    real minecraft!

---
+++ MC/MC/Camera.h
@@ -0,0 +1,48 @@
+#pragma once
+#pragma once
+#ifndef CAMERA_H
+#define CAMERA_H
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+enum Camera_Movement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+// Default camera values
+const float YAW = -90.0f;//偏航角
+const float PITCH = 0.0f;//俯仰角
+const float SPEED = 2.5f;//鼠标速度
+const float SENSITIVITY = 0.1f;//鼠标位移敏感度
+const float ZOOM = 45.0f;//缩放
+class Camera {
+public:
+	// camera Attributes
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+	// euler Angles
+	float Yaw;
+	float Pitch;
+	// camera options
+	float MovementSpeed;
+	float MouseSensitivity;
+	float Zoom;
+
+	Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch);
+	Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch);
+	glm::mat4 GetViewMatrix();
+	void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+	void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch);
+	void ProcessMouseScroll(float yoffset);
+private:
+	// calculates the front vector from the Camera's (updated) Euler Angles
+	void updateCameraVectors();
+};
+#endif // !CAMERA_H
